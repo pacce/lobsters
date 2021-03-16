9949c515652c05b4d4d792920d48935d04fa619e
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2021-03-16 01:15:56 -0300

    init: starts up a lobste.rs react native client
    Fetches data from lobste.rs json api using axios. Renders all titles
    using FlatLists

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [entries, setEntries] = useState([{key: 'a'}, {key: 'b'}]);
+    useEffect(() => {
+        axios.get('https://lobste.rs/hottest.json')
+            .then(xs => setEntries(
+                xs.data.map(x => ({key: x.short_id, title: x.title}))
+            ))
+            .catch(r => console.log(r))
+            ;
+    }, []);
+
+    return (
+        <View style={styles.container}>
+        <FlatList
+            data        = {entries}
+            renderItem  = {({item}) => <Text>{item.title}</Text>}
+        />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor : '#fafafa',
+        alignItems      : 'center',
+        justifyContent  : 'center',
+    },
 });
