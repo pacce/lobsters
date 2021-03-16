afe719004bf740d541c586c91762c5ff688ac259
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2021-03-16 01:25:38 -0300

    feat: pads android content with status bar height
    Android has a permanent status bar, displaying battery and signal
    status, that overlaps displayed content. Therefore, adds a platform
    check during css definition, padding content by the status bar height on
    android devices.

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const [entries, setEntries] = useState([{key: 'a'}, {key: 'b'}]);
+    const [entries, setEntries] = useState([]);
     useEffect(() => {
         axios.get('https://lobste.rs/hottest.json')
             .then(xs => setEntries(
@@ -30,5 +29,6 @@ const styles = StyleSheet.create({
         backgroundColor : '#fafafa',
         alignItems      : 'center',
         justifyContent  : 'center',
+        paddingTop      : Platform.OS === 'android' ? StatusBar.currentHeight : 0
     },
 });
