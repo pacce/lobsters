e98dbf77f02852af86b89c9de2a6b15b320ea385
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2021-03-20 18:00:13 -0300

    feat: renders all items as pure text
    It renders the following:
    
    ```
    Title
    username | hostname | comments
    ```

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { FlatList, Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
+import 'react-native-url-polyfill/auto';
+
+const Comments = ({entry}) => {
+    const renderComments = (count) => {
+        switch(count) {
+            case 0  : return "no comments";
+            case 1  : return "1 comment";
+            default : return count + " comments";
+        }
+    };
+    return <Text>{renderComments(entry.comments)}</Text>;
+};
+
+const Entry = ({entry}) => (
+    <View style={styles.item}>
+        <Text style={styles.storylink}>{entry.title}</Text>
+        <Text style={styles.information}>
+            {entry.username} | {entry.hostname} | <Comments entry={entry}/>
+        </Text>
+    </View>
+);
 
 export default function App() {
     const [entries, setEntries] = useState([]);
     useEffect(() => {
         axios.get('https://lobste.rs/hottest.json')
             .then(xs => setEntries(
-                xs.data.map(x => ({key: x.short_id, title: x.title}))
+                xs.data.map(function(x) {
+                    let domain = (new URL(x.url));
+                    console.log(x);
+                    return {
+                        key: x.short_id
+                        , title: x.title
+                        , hostname: domain.hostname
+                        , username: x.submitter_user.username
+                        , comments: x.comment_count
+                    };
+                })
             ))
             .catch(r => console.log(r))
             ;
@@ -17,7 +48,7 @@ export default function App() {
         <View style={styles.container}>
         <FlatList
             data        = {entries}
-            renderItem  = {({item}) => <Text>{item.title}</Text>}
+            renderItem  = {({item}) => <Entry entry={item}/>}
         />
         </View>
     );
@@ -31,4 +62,13 @@ const styles = StyleSheet.create({
         justifyContent  : 'center',
         paddingTop      : Platform.OS === 'android' ? StatusBar.currentHeight : 0
     },
+    storylink: {
+        fontFamily  : 'sans-serif',
+        fontSize    : 14,
+    },
+    information: {
+        fontFamily  : 'sans-serif',
+        fontSize    : 10,
+        color       : "#828282"
+    }
 });
